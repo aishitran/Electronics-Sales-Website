a7a0f60cadb79dd2740945799406061b012eb1bd
DarkRepulser026 <117032487+DarkRepulser026@users.noreply.github.com>
2025-04-08 13:12:49 +0700

    update insert_admin

--- insert_admin.php
+++ insert_admin.php
@@ -1,6 +1,10 @@
 <?php
-require_once 'app/models/ProductModel.php';
-$model = new ProductModel();
-$model->registerUser('Trần Thị B', 'ttb@example.com', 'password456', '0912345678', 'Hà Nội', 2);
+require_once 'app/models/AccountModel.php';
+require_once 'app/config/database.php';
+
+$database = new Database();
+$db = $database->getConnection();
+$model = new AccountModel($db);
+$model->registerUser('admin', 'admin@example.com', 'admin', '0912345678', 'Hà Nội', 2);
 echo "Admin inserted successfully!";
 ?>
